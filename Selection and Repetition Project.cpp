04cfde2cf1a6f6a28f6868c6c203ea5821670c12
Johnathan Shelton <js24634@email.vccs.edu>
2024-09-30 19:33:20 -0400

    Adding code and comments.

--- Selection and Repetition Project.cpp
+++ Selection and Repetition Project.cpp
@@ -1,8 +1,42 @@
-// Selection and Repetition Project.cpp : This file contains the Selection and Repetition Projects
-//
+//This program is part 2 of the calculator project switched nested if statements
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "This is the main branch of the Selection and Repetition Projects\n";
+using namespace std;
+
+int main() {
+    int num1, num2;
+    char op;
+
+            // Take input for the first number, operator, and second number
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+            // Using nested structure
+    if (op == '+') {
+        cout << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+    }
+    else if (op == '-') {
+        cout << num1 << " - " << num2 << " = " << (num1 - num2) << endl;
+    }
+    else if (op == '*') {
+        cout << num1 << " * " << num2 << " = " << (num1 * num2) << endl;
+    }
+    else if (op == '/') {
+        if (num2 == 0) {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+        else {
+            cout << num1 << " / " << num2 << " = " << (num1 / num2) << endl;
+        }
+    }
+    else {
+        cout << "Error: Unsupported operator." << endl;
+    }
+
+    return 0;
+}
