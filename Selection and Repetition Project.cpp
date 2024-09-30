2d89e29a0526411604aecd70ea6dd47cb2f46bef
Johnathan Shelton <js24634@email.vccs.edu>
2024-09-30 19:04:40 -0400

    Modify wording of main branch

--- Selection and Repetition Project.cpp
+++ Selection and Repetition Project.cpp
@@ -1,8 +1,8 @@
-// Selection and Repetition Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Selection and Repetition Project.cpp : This file contains the Selection and Repetition Projects
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "This is the main branch of the Selection and Repetition Projects\n";
